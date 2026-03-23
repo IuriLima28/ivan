aef5d39ecaa2cd83b444dabe0fbb19a9118cbed7
IURI D'OLIVEIRA DE LIMA <iuri_d_lima@estudante.sesisenai.org.br>
2026-03-23 15:16:55 -0300

    Finalizado

--- src/exercicio-10/exercicio.10.test.js
+++ src/exercicio-10/exercicio.10.test.js
@@ -1,4 +1,4 @@
-const { calcularCambio } = require('./exercicio-10');
+const calcularCambio = require('./exercicio-10');
 describe("Teste de investimento em moeda estrangeira", () => {
     test("Deve calcular corretamente o valor em reais", () => {
         const resultado = calcularCambio(1000, 5);
